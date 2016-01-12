780b74b00cade021146b708705e0d5b15f175e00
anerishah3110 <anerishah3110@gmail.com>
2016-01-11 21:35:17 -0800

    added C program files

---
+++ SimpleCPrograms/NAME_ADDRESS.C
@@ -0,0 +1,37 @@
+//W A P Name Address City Using printf
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int no1,no2;
+clrscr();
+printf("\n Name => Dhruv");
+printf("\n Address => 12,Dungarshinagar soc, Part-1, Bhatha");
+printf("\n City => Ahmedabad");
+getch();
+}
+ /*
+ Name => Dhruv
+ Address => 12,Dungarshinagar soc, Part-1, Bhatha
+ City => Ahmedabad
+ */
+
+
+
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
